2f2596cb2719b3fa4119d028afeaef8d1f64c723
Rafael Martins <rafaelplmartins@hotmail.com>
2018-11-23 05:45:29 -0200

    feat: add navigator

---
+++ src/navigation/index.js
@@ -0,0 +1,21 @@
+import Home from "../screens/Home"
+import React from 'react';
+import {
+  createStackNavigator,
+  createAppContainer
+} from 'react-navigation';
+
+const RootStack = createStackNavigator({
+  Home: Home
+},
+{
+  headerMode: "none"
+});
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
